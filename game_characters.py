d6410ff9badb89bb737988c703a52976313993ea
TechMaster-1701 <86656208+TechMaster-1701@users.noreply.github.com>
2021-11-21 11:36:05 +0530

    Create game_characters.py
    Characters

---
+++ game_characters.py
@@ -0,0 +1,24 @@
+ATTITUDES = [ANGER, HAPPINESS, SADNESS, TRUST]
+OBSTACLES = [TREES, ROCKS]
+class Characters:
+  def __init__:
+    #game characters definition
+    return 0
+  
+  def Jonathan():
+    ATTITUDE = ANGER
+    print("Survival Expert, Short Temepered")
+  
+  def Maria():
+    ATTITUDE_MARIA = [HAPPINESS, TRUST]
+    print("Kind, Lovable, Caring")
+    
+class JungleScene:
+  def Obstacles(OBSTACLE):
+    if OBSTACLE == "TREES":
+      print("Trees")
+      
+    elif OBSTACLE == "ROCKS":
+      print("Rocks")
+    
+    
